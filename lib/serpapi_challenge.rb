6fc95b3ccf6ef1b87ee1c8560be07bd0cdedd734
Marina Ferreira <marina.ferreira.dev@gmail.com>
2024-12-26 10:48:28 -0300

    Add app errors

--- lib/serpapi_challenge.rb
+++ lib/serpapi_challenge.rb
@@ -5,6 +5,28 @@ require_relative "serpapi_challenge/version"
 require_relative "google/artwork/crawler"
 
 module SerpapiChallenge
-  class Error < StandardError; end
-  # Your code goes here...
+  class BaseError < StandardError
+    ERROR_CODE = 400
+    def initialize
+      super
+    end
+
+    def description
+      "Something went wrong. Please, try again."
+    end
+
+    def message
+      "[#{self.class::ERROR_CODE}] #{description}"
+    end
+  end
+
+  class ArtistError < BaseError
+    def initialize
+      super
+    end
+
+    def description
+      "Artist not found"
+    end
+  end
 end
